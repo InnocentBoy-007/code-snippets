f02861f9699169820a3b337668d28182cfb08c2a
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-11-10 22:57:53 +0530

    Add files via upload
    auth middleware for jwt from cookies

---
+++ AuthMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = (req, res, next) => {
+    const token = req.cookies.adminToken; // since I'm using cookies
+
+    if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.admin = decoded; // Attach decoded payload (e.g., adminId) to request
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+};
