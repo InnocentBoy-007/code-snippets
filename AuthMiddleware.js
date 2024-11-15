fc945a0851083d3e36871efa4167012b50cd2519
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-11-15 12:34:18 +0530

    Update AuthMiddleware.js

--- AuthMiddleware.js
+++ AuthMiddleware.js
@@ -1,15 +1,24 @@
 import jwt from 'jsonwebtoken';
 
-export const authMiddleware = (req, res, next) => {
-    const adminToken = req.cookies.adminToken; // for admins
-    const clientToken = req.cookies.clientToken; // for clients
+// for admin authentication and authorization
+export const adminAuthMiddleware = (req, res, next) => {
+    const adminToken = req.cookies.adminToken; // since I'm using cookies
     if (!adminToken) return res.status(401).json({ message: 'Access denied. No admin token provided.' });
-    if (!clientToken) return res.status(401).json({ message: 'Access denied. No client token provided.' });
-
     try {
         const adminTokenDecoded = jwt.verify(adminToken, process.env.JWT_SECRET);
         req.admin = adminTokenDecoded;
+        next();
+    } catch (err) {
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+};
 
+// for client authentication and authorization
+export const clientAuthMiddleware = (req, res, next) => {
+    const clientToken = req.cookies.clientToken;
+    if (!clientToken) return res.status(401).json({ message: 'Access denied. No client token provided.' });
+
+    try {
         const clientTokenDecoded = jwt.verify(clientToken, process.env.JWT_SECRET);
         req.client = clientTokenDecoded;
         next();
