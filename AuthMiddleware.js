40ed75932e48b1ceb069bd5585abe42f421f8059
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-11-24 21:12:01 +0530

    Update AuthMiddleware.js
    Changed from cookie to headers

--- AuthMiddleware.js
+++ AuthMiddleware.js
@@ -1,28 +1,39 @@
 import jwt from 'jsonwebtoken';
 
-// for admin authentication and authorization
 export const adminAuthMiddleware = (req, res, next) => {
-    const adminToken = req.cookies.adminToken; // since I'm using cookies
+    // const adminToken = req.cookies.adminToken; // since I'm using cookies
+    const authHeader = req.headers['authorization'];
+    const adminToken = authHeader && authHeader.split(' ')[1];
     if (!adminToken) return res.status(401).json({ message: 'Access denied. No admin token provided.' });
     try {
         const adminTokenDecoded = jwt.verify(adminToken, process.env.JWT_SECRET);
         req.admin = adminTokenDecoded;
+
         next();
     } catch (err) {
         res.status(400).json({ message: 'Invalid token.' });
     }
 };
 
-// for client authentication and authorization
 export const clientAuthMiddleware = (req, res, next) => {
-    const clientToken = req.cookies.clientToken;
+    // console.log("Headers-->", req.headers); // use this for logging the contents of the headers
+
+    const authHeader = req.headers['authorization'];
+    const clientToken = authHeader && authHeader.split(' ')[1];
+    // console.log("Client Token inside the auth middleware-->", clientToken);
+
+    if (clientToken === null) return res.status(401).json({ message: "Token is null! - auth backend" });
+
     if (!clientToken) return res.status(401).json({ message: 'Access denied. No client token provided.' });
 
     try {
         const clientTokenDecoded = jwt.verify(clientToken, process.env.JWT_SECRET);
         req.client = clientTokenDecoded;
+        // console.log("Decoded token--->", req.client);
+        // req.clientToken = clientToken;
         next();
     } catch (err) {
+        console.log("Auth error --->", err);
         res.status(400).json({ message: 'Invalid token.' });
     }
 };
