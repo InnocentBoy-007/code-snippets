bfa6e1062683f862970d32bd383235aec266140d
InnocentBoy <141760554+InnocentBoy-007@users.noreply.github.com>
2024-12-09 14:26:20 +0530

    Update AuthMiddleware.js

--- AuthMiddleware.js
+++ AuthMiddleware.js
@@ -1,39 +1,38 @@
 import jwt from 'jsonwebtoken';
 
 export const adminAuthMiddleware = (req, res, next) => {
-    // const adminToken = req.cookies.adminToken; // since I'm using cookies
     const authHeader = req.headers['authorization'];
     const adminToken = authHeader && authHeader.split(' ')[1];
+
     if (!adminToken) return res.status(401).json({ message: 'Access denied. No admin token provided.' });
     try {
-        const adminTokenDecoded = jwt.verify(adminToken, process.env.JWT_SECRET);
-        req.admin = adminTokenDecoded;
+        const verifyTokenAndDecoding = jwt.verify(adminToken, process.env.JWT_SECRET);
+        req.adminId = verifyTokenAndDecoding.adminId;
 
         next();
     } catch (err) {
-        res.status(400).json({ message: 'Invalid token.' });
+        console.error("Error in admin auth middleware", err);
+        if (err.name == 'JsonWebTokenError') return res.status(409).json({ message: "Invalid token! Authorization denied! - backend" });
+        if (err.name == 'TokenExpiredError') return res.status(401).json({ message: "Token expired! Authrorization denied! - backend" })
+        return res.status(500).json({ message: "An unexpected error occured while trying to verify the token - backend" });
     }
 };
 
-export const clientAuthMiddleware = (req, res, next) => {
-    // console.log("Headers-->", req.headers); // use this for logging the contents of the headers
 
+export const clientAuthMiddleware = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const clientToken = authHeader && authHeader.split(' ')[1];
-    // console.log("Client Token inside the auth middleware-->", clientToken);
-
-    if (clientToken === null) return res.status(401).json({ message: "Token is null! - auth backend" });
 
     if (!clientToken) return res.status(401).json({ message: 'Access denied. No client token provided.' });
-
     try {
         const clientTokenDecoded = jwt.verify(clientToken, process.env.JWT_SECRET);
-        req.client = clientTokenDecoded;
-        // console.log("Decoded token--->", req.client);
-        // req.clientToken = clientToken;
+        req.clientId = clientTokenDecoded.clientId;
+
         next();
     } catch (err) {
-        console.log("Auth error --->", err);
-        res.status(400).json({ message: 'Invalid token.' });
+        console.error("Error in admin auth middleware", err);
+        if (err.name == 'JsonWebTokenError') return res.status(409).json({ message: "Invalid token! Authorization denied! - backend" });
+        if (err.name == 'TokenExpiredError') return res.status(401).json({ message: "Token expired! Authrorization denied! - backend" })
+        return res.status(500).json({ message: "An unexpected error occured while trying to verify the token - backend" });
     }
 };
